9f1d9687973888524f0a202c9b27afb690ec4a98
anderuraga <ander.uraga@gmail.com>
2020-10-23 08:12:54 +0200

    Nuevo ejercicio

---
+++ src/com/ipartek/apps/AppSeries.java
@@ -0,0 +1,22 @@
+package com.ipartek.apps;
+
+/**
+ * Segun comience el metodo meain crear 5 series dentro de un arrayList.
+ * <a href="http://www.sensacine.com/series/top/" target="_blank">Mirar este enlace para crear las series </a>
+ * Pedir por consola los datos de una serie nueva<br>
+ * Crear objeto o instancia<br>
+ * Guardar el objeto en un ArrayList<br>
+ * Listar todas las series.<br>
+ * Dar la posibilidad de buscar todas las series de una plataforma concreta<br>
+ * 
+ * @author ur00
+ *
+ */
+public class AppSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/ipartek/pojo/Serie.java
@@ -0,0 +1,21 @@
+package com.ipartek.pojo;
+
+/**
+ * Una serie de TV consta de los siguientes atributos:
+ * 
+ * <ul>
+ * <li>Nombre</li>
+ * <li>numTemporadas</li>
+ * <li>duracion (minutos)</li>
+ * <li>plataforma(netflix,plex,hbo,moviestar,...)</li> 
+ * </ul>
+ * 
+ * El constructor debere pasarse siempre el nombre de forma obligatoria..
+ * El resto de campos se iniciaalzan a 0 y la plataforma especificar "internet"
+ * 
+ * @author ur00
+ *
+ */
+public class Serie {
+
+}
