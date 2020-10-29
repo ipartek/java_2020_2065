1ba5336286d756e9322108157b3d1fb4cb26e8f9
anderuraga <ander.uraga@gmail.com>
2020-10-29 08:29:28 +0100

    nueva actividad

---
+++ src/mf0227/uf2404/actividad2/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Definir una interfaz IConducible con los métodos arrancar y parar 
+ * Crear clase Vehiculo ( color, matricula ) para el Coche ( marca, modelo, potencia, cilindrada) 
+ * Realizar un Diagrama de Clases
+ */
+package mf0227.uf2404.actividad2;
