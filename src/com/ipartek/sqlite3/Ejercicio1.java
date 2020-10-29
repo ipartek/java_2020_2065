0e1e639182d40bce220ca90cc50e16bf3da66a60
anderuraga <ander.uraga@gmail.com>
2020-10-29 13:36:39 +0100

    sqlite connect

---
+++ src/com/ipartek/sqlite3/Ejercicio1.java
@@ -0,0 +1,30 @@
+package com.ipartek.sqlite3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection conn = DriverManager.getConnection("jdbc:sqlite:C:/sqlite/concesionario.db");
+		
+		String sql = "SELECT * FROM coche;";		
+		PreparedStatement pst = conn.prepareStatement(sql);
+		ResultSet rs = pst.executeQuery();
+		
+		while( rs.next() ) {
+			System.out.println( rs.getInt("id") + rs.getString("nombre"));
+			
+		}
+		
+		
+		
+		System.out.println("terminamos");
+
+	}
+
+}
