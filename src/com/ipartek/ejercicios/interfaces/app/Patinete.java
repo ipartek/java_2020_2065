5d964b6adb701be076b20fe67d526beb35d2eb3f
anderuraga <ander.uraga@gmail.com>
2020-10-30 08:55:39 +0100

    bbdd perros

--- src/com/ipartek/ejercicios/interfaces/app/Patinete.java
+++ src/com/ipartek/ejercicios/interfaces/app/Patinete.java
@@ -1,6 +1,6 @@
 package com.ipartek.ejercicios.interfaces.app;
 
-public class Patinete implements IVehiculo {
+public class Patinete implements IVehiculo, AutoCloseable {
 
 	// Atributos, deben ser siempre privados
 	private int velocidadActual;
@@ -77,6 +77,13 @@ public class Patinete implements IVehiculo {
 		}else {
 			this.estaArrancado = false;
 		}		
+	}
+
+	@Override
+	public void close() throws Exception {
+		System.out.println("Cerramos el patinete");
+		apagar();
+		
 	}
 
 
--- src/com/ipartek/sqlite3/Ejercicio2.java
+++ src/com/ipartek/sqlite3/Ejercicio2.java
@@ -4,30 +4,47 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
+import java.util.ArrayList;
 
 import com.ipartek.ejercicios.interfaces.app.Patinete;
+import com.ipartek.pojo.Perro;
 
-public class Ejercicio1 {
+public class Ejercicio2 {
 
 	public static void main(String[] args) throws Exception {
 
-		String sql = "SELECT * FROM coche ORDER BY nombre ASC;";
+		String sql = "SELECT * FROM perro ORDER BY nombre ASC;";
+		ArrayList<Perro> perros = new ArrayList();
 
 		// Todo lo que metamos dentro de los () del try se cierra automaticamente al
 		// terminar el try
 		// Esto es posible porque los recursos implementan la interfaz AutoClosable
-		try (Connection conn = DriverManager.getConnection("jdbc:sqlite:ddbb/concesionario.db");
+		try (Connection conn = DriverManager.getConnection("jdbc:sqlite:ddbb/perrera.db");
 				PreparedStatement pst = conn.prepareStatement(sql);
 				ResultSet rs = pst.executeQuery();
 				Patinete patin = new Patinete();
 				) {
 
+			
+			
 			while (rs.next()) {
-				System.out.println(rs.getInt("id") + " " + rs.getString("nombre"));
+				
+				Perro p = new Perro();
+				p.setId( rs.getInt("id"));
+				p.setNombre( rs.getString("nombre"));
+				p.setRaza( rs.getString("raza"));
+				p.setPeso( rs.getFloat("peso"));
+				p.setVacunado( rs.getBoolean("vacunado"));
+				p.setHistoria( rs.getString("historia"));
+				perros.add(p);
 
 			}
 
+			
+			for (Perro perro : perros) {
+				System.out.println(perro);
+			}
+			
 		} // try, se cierran Connection, PreparedStatement, ResultSet y Patinete
 
 		System.out.println("terminamos");
